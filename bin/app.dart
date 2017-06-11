2e8d75e798554276e9bf334de69c9548b0760384
Srinivas Dhanwada <dhanwads@gmail.com>
2017-06-11 02:38:47 -0500

    Implement Server Layer API for data requests
    This commit adds custom request handlers for the various CRUD operations
    involved with manipulating heroes. The app now queries the api/ url to
    perform the operations so the server can respond appropriately. This
    moves the memory persistence from in-memory of the browser to in-memory
    of the server (e.g. data is now persisted across page refreshes).
    
    This is the next step in moving memory persistence to a database level.
    
    Ticket/Issue: N/A

--- bin/app.dart
+++ bin/app.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:express/express.dart';
 
 import 'redirect_file_handler.dart';
+import 'hero.dart';
+import 'mock_heroes.dart';
 
 main() {
   runApp();
@@ -9,24 +13,81 @@ main() {
 runApp() {
   const PATH = 'public/build/web';
 
+  List<Hero> _heroes = <Hero>[]..addAll(mockHeroes);
+  int _nextId = _heroes.map((h) => h.id).fold(0, max) + 1;
+
   var app = new Express()
     ..use(new RedirectFileHandler(PATH))
     ..get('/api/heroes', (ctx) {
+      var name = ctx.requestedUri.queryParameters['name'];
+      if(name != null) {
+        ctx.sendJson({
+          'data': _heroes.where((h) => h.name.contains(name)).toList()
+        }, httpStatus: 200);
+      } else {
+        ctx.sendJson({
+          'data': _heroes.map((h) => h.toJson()).toList()
+        }, httpStatus: 200);
+      }
+    })
+    ..post('/api/heroes', (ctx) async {
+      var jsonData = await ctx.readAsJson();
+      var name = jsonData['name'];
+      if(name == null) {
+        ctx.notFound('Invalid params!');
+        return;
+      }
+
+      var newHero = new Hero(_nextId++, name);
+      _heroes.add(newHero);
       ctx.sendJson({
-        'data': [
-          {'id': 11, 'name': 'Mr. Nice'},
-          {'id': 12, 'name': 'Narco'},
-          {'id': 13, 'name': 'Bombasto'},
-          {'id': 14, 'name': 'Celeritas'},
-          {'id': 15, 'name': 'Magneta'},
-          {'id': 16, 'name': 'RubberMan'},
-          {'id': 17, 'name': 'Dynamo'},
-          {'id': 18, 'name': 'Dr. IQ'},
-          {'id': 19, 'name': 'Magma'},
-          {'id': 20, 'name': 'Tornado'},
-        ]
+        'data': newHero.toJson()
+      }, httpStatus: 200);
+    })
+    ..get('/api/heroes/:id', (ctx) {
+      int id = validateId(ctx);
+      if(id == null) { return; }
+
+      var hero = _heroes.firstWhere((h) => h.id == id, orElse: () => null);
+      if(hero != null) {
+        ctx.sendJson({
+          'data': hero.toJson()
+        }, httpStatus: 200);
+      } else {
+        ctx.notFound('Hero $id does not exist');
+      }
+    })
+    ..put('/api/heroes/:id', (ctx) async {
+      int id = validateId(ctx);
+      if(id == null) { return; }
+
+      var jsonData = await ctx.readAsJson();
+      var heroChanges = new Hero.fromJson(jsonData);
+      var hero = _heroes.firstWhere((h) => h.id == heroChanges.id, orElse: () => null);
+      if(hero != null) {
+        hero.name =  heroChanges.name;
+        ctx.sendJson({
+          'data': heroChanges.toJson()
+        }, httpStatus: 200);
+      }
+    })
+    ..delete('/api/heroes/:id', (ctx) async {
+      int id = validateId(ctx);
+      if(id == null) { return; }
+
+      _heroes.removeWhere((h) => h.id == id);
+      ctx.sendJson({
+        'data': ''
       }, httpStatus: 200);
     });
 
   app.listen('127.0.0.1', 8000);
+}
+
+int validateId(HttpContext ctx) {
+  int id = int.parse(ctx.params['id'], onError: (str) {
+    ctx.notFound('Invalid id: $str');
+    return null;
+  });
+  return id;
 }
